68a630ed9c7918f78b50238a5464a1b5e8594517
Tayyab-it <tayyabaman80@gmail.com>
2022-05-27 09:37:19 +0500

    Loops for,while,do while,forEach forArray iter..

---
+++ loops.java
@@ -0,0 +1,44 @@
+public class loops{
+    public static void main(String[] args) {
+    /*     //For loop
+        for(int i=0;i<10;i++){
+           System.out.println(i);
+        } 
+    */
+
+
+
+     /*
+       //While Loop  
+       Integer n=0;
+        while(n<10){
+            System.out.println(n);
+            n++;
+        }
+     */
+
+
+
+    /*  //Do While Loop
+        Integer n=10;
+        do{
+            System.out.println(n);
+            n--;
+        }
+        while(n>10); 
+    */
+
+    /* 
+        //Array
+        Integer arr[]={1,2,3,4};
+        //ForEach Loop
+        for (Integer n : arr) {
+            System.out.println(n);
+        }
+    */
+
+    //
+
+
+    }
+}
