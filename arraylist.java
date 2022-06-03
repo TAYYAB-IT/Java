7e6bfac69cadf90a6ea34463c9264642b4c3da9c
Tayyab-it <tayyabaman80@gmail.com>
2022-06-03 18:09:43 +0500

    CRUD methods with ArrayList

--- arraylist.java
+++ arraylist.java
@@ -1,10 +1,19 @@
 import java.util.ArrayList;
-
+/* 
+ In arrayList a new array is replaced with a newer array 
+ & Old one array is removed/deleted
+ That's why better to use linked list 
+ */
 public class arraylist {
     public static void main(String[] args) {
         ArrayList<String> namesList = new ArrayList<String>();
         namesList.add("Ali");
         namesList.add("Ahmed");
+        //namesList.get(index)
+        //namesList.set(index, element)
+        // namesList.add(index, element);
+       // namesList.remove(index)
+       //namesList.clear(); //clear whole arraylist
         //namesList.size() 2
         //namesList[0] // Ali
         for (String name : namesList) {
