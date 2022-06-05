bb771bb9b14dfa7da8804f120d9d682ef17d7684
Tayyab-it <tayyabaman80@gmail.com>
2022-06-05 19:34:43 +0500

    linkedlist operations

---
+++ linkedlist.java
@@ -0,0 +1,27 @@
+import java.util.LinkedList;
+public class linkedlist{
+public static void main(String[] args) {
+    LinkedList<String> list=new  LinkedList<String>();
+    list.add("Tayyab");
+    list.add("Ali");
+    list.add("Qasim");
+    //list.add(value) //Add
+    //list.clear(); //Clear the whole Linked List
+   // System.out.println(list.remove()); //Remove & return the removed head node of linked list
+    //list.addFirst(e);
+    //list.addLast(e);
+    //list.add(index, element);
+    //list.removeLast()
+    //list.contains(o)
+    //list.get(index)
+   // list.indexOf(o);//First occurence index
+  // list.push(e); //Stack Push
+  //list.pop(); //Stack pop
+  //list.remove(index)
+  //list.remove(o); //first occureence removed
+  //Iterate A List
+/*   for (String string : list) {
+      System.out.println(string);
+  } */
+}
+}
