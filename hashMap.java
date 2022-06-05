580b4dd7be5f77a475bdc6b4d454d1c78e6c88c6
Tayyab-it <tayyabaman80@gmail.com>
2022-06-05 19:20:07 +0500

    HashMap All Operations

--- hashMap.java
+++ hashMap.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Map.Entry;
 public class hashMap {
     static HashMap<String,Float> dict=new HashMap<String,Float>();
 
@@ -17,6 +18,19 @@ while(true){
     marks=Float.parseFloat(System.console().readLine());
     dict.put(roll_no, marks);
  }
+}
+//dict.containsKey(key); //Check Either a key exist or not
+//dict.containsValue(value); //Check Either a value exist or not
+//dict.clear();// Clear the whole HashMap Data
+//dict.keySet();// Get Key Set of HashMap 
+//dict.put(key, value); // Add A new Or Update key value
+//dict.remove(key);  //Remove a Value by key
+//dict.get(key);   //Get A value by key
+//dict.values()     //Get All Values
+//Iterate A HashMap //dict.entrySet()
+System.out.println("----------------------------------------------");
+for(Entry <String,Float> entry:dict.entrySet()){
+System.out.println(entry.getKey()+" : "+entry.getValue());
 }
 }
 }
