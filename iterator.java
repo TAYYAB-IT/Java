9dd923bdd5a40bb4f0db5823a28fa44e7abd7b25
Tayyab-it <tayyabaman80@gmail.com>
2022-06-06 20:21:40 +0500

    iterator used instead Loops

---
+++ iterator.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class iterator {
+    public static void main(String[] args) {
+        ArrayList <Integer> list=new ArrayList<Integer>();
+        list.add(1);
+        list.add(3);
+        list.add(5);
+        list.add(7);
+        list.add(9);
+       /*  Iterator it=list.listIterator();
+        while(it.hasNext()){
+            System.out.println(it.next());
+        } */
+    }
+}
