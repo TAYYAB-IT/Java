26ecf83b52c89268d350404e2a9159dc39632cbc
Tayyab-it <tayyabaman80@gmail.com>
2022-06-06 20:15:04 +0500

    HashSet operations

---
+++ hashSet.java
@@ -0,0 +1,16 @@
+import java.util.HashSet;
+public class hashSet {
+    public static void main(String[] args) {
+   //Get unique values like set in mathematics
+        HashSet<Integer>set=new HashSet<Integer>();
+        //set.add(value); //Add A Value
+       //set.contains(o);//Check Either a value exist in set or not
+       //set.remove(o); //remove a value
+       //set.clear(); //clear the  whole set
+       //set.size(); //get size of set
+       //Iterate a Set
+      /*  for (Integer integer : set) {
+           System.out.println(integer);
+       } */
+    }
+}
