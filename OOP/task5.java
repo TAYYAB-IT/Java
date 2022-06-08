0e87470473a8398d90db0c30de83a068d3c1f396
Tayyab-it <tayyabaman80@gmail.com>
2022-06-08 15:17:53 +0500

    Student grades handling

---
+++ OOP/task5.java
@@ -0,0 +1,18 @@
+import Classes.Student;
+import java.util.HashMap;
+import java.util.Map.Entry;
+public class task5 {
+    public static void main(String[] args) {
+
+            Student s = new Student("1901-1", "Tayyab", 20);
+            s.AddResult("Android", 80);
+            s.AddResult("AI", 70);
+            s.AddResult("ML", 60);
+           System.out.println("Your CGPA is "+s.GetCgpa());
+           HashMap<String,Character> grades= s.GetGrades();
+           System.out.println("----------------Grades-----------------");
+           for (Entry<String,Character> entry : grades.entrySet()) {
+               System.out.println(entry.getKey()+"::"+entry.getValue());
+           }
+    }
+}
